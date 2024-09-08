7601029942d1c960435dc1e2fe02b6f6c090be3e
vermarjun <vermarjun26@gmail.com>
2024-09-08 15:23:56 +0530

    update

--- Questions/Lc-1275_Winner_of_Tic_Tac_Toe.cpp
+++ Questions/Lc-1275_Winner_of_Tic_Tac_Toe.cpp
@@ -7,29 +7,29 @@ string tictactoe(vector<vector<int>>& moves) {
     // A
     vector<int> row(3,0);
     vector<int> col(3,0);
-    int d = 0;
-    int rd = 0;
+    int Dia = 0;
+    int reverseDia = 0;
     int size = moves.size();
     for (int i= 0; i < size; i+=2){
         row[moves[i][0]]++;
         col[moves[i][1]]++;
         if (row[moves[i][0]]==3 || col[moves[i][1]]==3) return "A";
-        if (moves[i][0] == moves[i][1]) d++;
-        if (moves[i][0] + moves[i][1] == 2) rd++;
-        if (d == 3 || rd == 3) return "A"; 
+        if (moves[i][0] == moves[i][1]) Dia++;
+        if (moves[i][0] + moves[i][1] == 2) reverseDia++;
+        if (Dia == 3 || reverseDia == 3) return "A"; 
     }
     // B
-    vector<int> rowB(3,0);
-    vector<int> colB(3,0);
-    d = 0;
-    rd = 0;
+    row.clear();
+    col.clear();
+    Dia = 0;
+    reverseDia = 0;
     for (int i = 1; i < size; i+=2){
-        rowB[moves[i][0]]++;
-        colB[moves[i][1]]++;
-        if (rowB[moves[i][0]]==3 || colB[moves[i][1]]==3) return "B";
-        if (moves[i][0] == moves[i][1]) d++;
-        if (moves[i][0] + moves[i][1] == 2) rd++;
-        if (d == 3 || rd == 3) return "B";
+        row[moves[i][0]]++;
+        col[moves[i][1]]++;
+        if (row[moves[i][0]]==3 || col[moves[i][1]]==3) return "B";
+        if (moves[i][0] == moves[i][1]) Dia++;
+        if (moves[i][0] + moves[i][1] == 2) reverseDia++;
+        if (Dia == 3 || reverseDia == 3) return "B";
     }
     if (size == 9) return "Draw";
     return "Pending";
